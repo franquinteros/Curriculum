365b9b724ced15dbf96211ae461871939f683135
franquinteros <frandecholila@gmail.com>
2023-10-19 16:24:14 -0300

    http client

--- angular/src/app/proyectos/proyectos.component.ts
+++ angular/src/app/proyectos/proyectos.component.ts
@@ -5,15 +5,21 @@ import { CvService } from '../servicios/cv.service';
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
-proyectos: any;
-  constructor(private serviciocv: CvService) { }
 
-  ngOnInit(): void {
+export class ProyectosComponent {
+  obtenerProyectos: any;
+  constructor(private serviciocv: CvService) {
     this.serviciocv.obtenerProyectos().subscribe(
-      data => {
-        console.log(data["proyectos"][0]);
-        this.proyectos = data["proyectos"][0];
+      {
+        next: (data) => {
+          this.obtenerProyectos = data["proyectos"];
+          console.log("proyectos ");
+          console.log(this.obtenerProyectos);
+        },
+        error: (err) => {
+          alert("Se ha producido un error. Por favor, intente nuevamente.");
+          console.error(err)
+        }
       }
     )
   }
